0ffce9a1121f83e153952f8c6b33bb2bfbe3d2f3
Vignesh P <105636388+vick-254@users.noreply.github.com>
2023-06-15 15:38:41 +0530

    ReverseArray_GivenIndex

---
+++ ReverseArray_GivenIndex.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void reverseArray(vector<int> &arr, int m)
+{
+
+    int s = m + 1;
+    int e = arr.size() - 1;
+    while (s < e)
+    {
+
+        swap(arr[s], arr[e]);
+        s++;
+        e--;
+    }
+}
+
+int main()
+{
+
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7};
+    reverseArray(arr, 3);
+    for (int i : arr)
+    {
+        cout << i << " ";
+    }
+}
