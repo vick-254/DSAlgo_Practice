98828d26995341068b2f0b6ce3b7a23c6f8818d5
Vignesh P <105636388+vick-254@users.noreply.github.com>
2023-06-22 15:42:32 +0530

    factorial

---
+++ Phase 2/Recursion/factorial.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+
+    if (n == 0)
+        return 1;
+
+    return n * factorial(n - 1);
+}
+
+int main()
+{
+
+    int ans = factorial(5);
+    cout << ans;
+}
