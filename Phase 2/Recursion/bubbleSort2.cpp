3b8a55a7ca292673a30cbd29e20d4b87a75e81a1
Vignesh P <105636388+vick-254@users.noreply.github.com>
2023-06-25 11:13:15 +0530

    bubbleSort2

---
+++ Phase 2/Recursion/bubbleSort2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void bubbleSort(vector<int> &arr, int r, int c)
+{
+    if (r == 0)
+    {
+
+        return;
+    }
+
+    if (c < r)
+    {
+        if (arr[c] > arr[c + 1])
+        {
+            swap(arr[c], arr[c + 1]);
+        }
+        return bubbleSort(arr, r, c + 1);
+    }
+    else
+    {
+        return bubbleSort(arr, r - 1, 0);
+    }
+}
+
+int main()
+{
+    vector<int> arr = {99, 7, 8, 33, 6, 5};
+    bubbleSort(arr, 6, 6);
+    for (int i : arr)
+    {
+        cout << i << " ";
+    }
+}
