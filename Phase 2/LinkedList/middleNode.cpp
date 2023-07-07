6d8583466e08000563983e2485a5a5fc9e1ca362
Vignesh P <105636388+vick-254@users.noreply.github.com>
2023-07-07 20:43:48 +0530

    middleNode

---
+++ Phase 2/LinkedList/middleNode.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *middleNode(ListNode *head)
+    {
+        ListNode *fast = head;
+        ListNode *slow = head;
+        int count = 0;
+        while (fast && fast->next)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+
+        return slow;
+    }
+};
