9461a5d105a91fc2c9b45bfa352e501d9de42ee7
Vignesh P <105636388+vick-254@users.noreply.github.com>
2023-07-12 01:27:41 +0530

    zigZagTraversal

---
+++ Phase 2/TUF_Trees/zigZagTraversal.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class TreeNode
+{
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+};
+
+vector<vector<int>> zigzagLevelOrder(TreeNode *root)
+{
+    vector<vector<int>> result;
+    if (root == NULL)
+    {
+        return result;
+    }
+
+    queue<TreeNode *> q;
+    q.push(root);
+    bool flag = true;
+    while (!q.empty())
+    {
+        int size = q.size();
+        vector<int> ans(size);
+
+        for (int i = 0; i < size; i++)
+        {
+            TreeNode *node = q.front();
+            q.pop();
+
+            int index = (flag) ? i : size - i - 1;
+
+            ans[index] = node->val;
+
+            if (node->left)
+                q.push(node->left);
+            if (node->right)
+                q.push(node->right);
+        }
+        flag = !flag;
+        result.push_back(ans);
+    }
+    return result;
+}
