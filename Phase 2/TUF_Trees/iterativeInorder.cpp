50ed30f1a0f3860990e2f44bec5036e9cda6d4fd
Vignesh P <105636388+vick-254@users.noreply.github.com>
2023-07-11 18:50:55 +0530

    iterativeInorder

---
+++ Phase 2/TUF_Trees/iterativeInorder.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class TreeNode
+{
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+};
+
+vector<int> inorderTraversal(TreeNode *root)
+{
+    vector<int> ans;
+    if (root == NULL)
+        return ans;
+    TreeNode *node = root;
+    stack<TreeNode *> st;
+
+    while (true)
+    {
+        if (node)
+        {
+            st.push(node);
+            node = node->left;
+        }
+        else
+        {
+            if (st.empty())
+                break;
+            node = st.top();
+            st.pop();
+            ans.push_back(node->val);
+            node = node->right;
+        }
+    }
+    return ans;
+}
