1fa2bf4e10538c1363800856b4a183bdd605da46
Vignesh P <105636388+vick-254@users.noreply.github.com>
2023-07-17 22:28:02 +0530

    BSTfromPreorder

---
+++ Phase 2/BST/BSTfromPreorder.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode *bstFromPreorder(vector<int> &preorder)
+{
+    int i = 0;
+    return helper(preorder, i, INT_MAX);
+}
+
+TreeNode *helper(vector<int> &preorder, int &i, int bound)
+{
+    if (i == preorder.size() || preorder[i] > bound)
+        return NULL;
+
+    TreeNode *node = new TreeNode(preorder[i++]);
+
+    node->left = helper(preorder, i, node->val);
+    node->right = helper(preorder, i, bound);
+
+    return node;
+}
