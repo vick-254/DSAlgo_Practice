dc88c56e80e0cd2469fc399a1c2ececbe10a95fb
Vignesh P <105636388+vick-254@users.noreply.github.com>
2023-08-06 11:17:00 +0530

    6_uniqePaths_tab

---
+++ Phase 2/DP/6_uniquePaths_tab.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+
+public:
+    int uniquePaths(int m, int n)
+    {
+
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        dp[0][0] = 1;
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i == 0 && j == 0)
+                    continue;
+                else
+                {
+                    int up = 0;
+                    int left = 0;
+                    if (i > 0)
+                        up = dp[i - 1][j];
+
+                    if (j > 0)
+                        left = dp[i][j - 1];
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
