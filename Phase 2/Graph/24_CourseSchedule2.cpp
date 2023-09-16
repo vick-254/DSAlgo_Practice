68439ea0bd8ea8566dd425ac2a34922ee88497b7
Vignesh P <105636388+vick-254@users.noreply.github.com>
2023-09-16 23:17:08 +0530

    course schedule 2

---
+++ Phase 2/Graph/24_CourseSchedule2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> findOrder(int V, int m, vector<vector<int>> prerequisites)
+    {
+        vector<int> adj[V];
+        for (auto it : prerequisites)
+        {
+            adj[it[1]].push_back(it[0]);
+        }
+
+        queue<int> q;
+        vector<int> indegree(V, 0);
+        vector<int> ans;
+
+        for (int i = 0; i < V; i++)
+        {
+            for (auto it : adj[i])
+            {
+                indegree[it]++;
+            }
+        }
+
+        for (int i = 0; i < V; i++)
+        {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for (auto it : adj[node])
+            {
+                indegree[it]--;
+                if (indegree[it] == 0)
+                    q.push(it);
+            }
+        }
+
+        if (ans.size() < V)
+        {
+            vector<int> ans;
+            return ans;
+        }
+        else
+            return ans;
+    }
+};
