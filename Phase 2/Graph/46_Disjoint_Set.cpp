97c1b05b6f1f22c54b2b82ff40f47722f050c534
Vignesh P <105636388+vick-254@users.noreply.github.com>
2023-10-10 11:53:48 +0530

    union by rank

---
+++ Phase 2/Graph/46_Disjoint_Set.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class DisjointSet
+{
+    vector<int> rank, parent;
+
+public:
+    DisjointSet(int n)
+    {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+
+        for (int i = 0; i <= n; i++)
+        {
+            parent[i];
+        }
+    }
+
+    int findUPar(int node)
+    {
+        if (node == parent[node])
+        {
+            return node;
+        }
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return;
+
+        else if (rank[ulp_u] < rank[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u])
+        {
+            parent[ulp_v] = ulp_u;
+        }
+        else if (rank[ulp_u] = rank[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            rank[ulp_v]++;
+        }
+    }
+};
